e652e8c947e81462259ec9c7620304671606f7e7
Marko Naumovic <m.naumovic@virtualvaults.com>
2023-01-11 12:48:37 +0100

    Init

---
+++ BookStore/BookStore/PageObject/BookPage.cs
@@ -0,0 +1,25 @@
+﻿namespace BookStore.PageObject;
+
+public class BookPage
+{
+    private readonly IPage _page;
+
+    private ILocator ButtonLogin => _page.Locator("#login");
+    private ILocator ButtonNewUser => _page.Locator("#newUser");
+
+    public async Task ClickOnLoginButton()
+    {
+        await ButtonLogin.ClickAsync();
+    }
+
+    public async Task ClickOnNewUserButton()
+    {
+        await ButtonNewUser.ClickAsync();
+    }
+
+    public BookPage(IPage page)
+    {
+        _page = page;
+    }
+}
+
---
+++ BookStore/BookStoreTestSet/Global.cs
@@ -0,0 +1,3 @@
+global using NUnit.Framework;
+global using BookStore.PageObject;
+global using Microsoft.Playwright.NUnit;   

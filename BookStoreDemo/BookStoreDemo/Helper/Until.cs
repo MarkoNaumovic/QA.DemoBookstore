6391d1ceea2e5aefbd64f7c858dfe4416176d903
Marko Naumovic <m.naumovic@virtualvaults.com>
2023-01-18 13:21:57 +0100

    Update

---
+++ BookStoreDemo/BookStoreDemo/Helper/Until.cs
@@ -0,0 +1,24 @@
+﻿namespace BookStoreDemo.Helper;
+
+public class Until
+{
+    private readonly IPage _page;
+
+    public Until(IPage page)
+    {
+        _page = page;
+    }
+
+    public async Task ScreenshotWhenTestsFails()
+    {
+        var testContext = TestContext.CurrentContext;
+
+        if (testContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Failed)
+        {
+            var uniqueName = Path.Combine(Path.GetTempPath(), $"{testContext.Test.Name}_{DateTime.Now:yyyy_MM_dd_HH_mm_ssss}.png");
+            await _page.ScreenshotAsync(new PageScreenshotOptions { Path = uniqueName });
+            TestContext.AddTestAttachment(uniqueName, testContext.Test.MethodName);
+        }
+    }
+}
+

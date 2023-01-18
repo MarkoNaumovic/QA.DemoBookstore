aa47c737e02987b2fc54a4657d69c6f2a24725f7
Marko Naumovic <m.naumovic@virtualvaults.com>
2023-01-18 13:31:34 +0100

    Add PageBook

---
+++ BookStoreDemo/BookStoreDemo/PageObject/MyPageBook.cs
@@ -0,0 +1,26 @@
+﻿using BookStoreDemo.Helper;
+using BookStoreDemo.PlaywrightDriver;
+using Microsoft.Playwright.NUnit;
+namespace BookStoreDemo.PageObject;
+
+public class MyPageBook: PageTest
+{
+    public Until _utilPage;
+    public PwDriver _wpDriver;
+
+    [SetUp]
+    public void BeforeEachTest()
+    {
+        _wpDriver = new PwDriver(Page);
+        _utilPage = new Until(Page);
+    }
+
+
+    [TearDown]
+    public async Task WebAppPageAfterEachTest()
+    {
+        await _utilPage.ScreenshotWhenTestsFails();
+        await Page.CloseAsync();
+    }
+}
+
